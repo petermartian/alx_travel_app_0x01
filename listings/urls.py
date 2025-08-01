1479b879c6bd65201a715f8fa45e3ed46f81e18b
Peter Martian <petmartian@gmail.com>
2025-08-01 18:08:13 +0100

    Update urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -1,11 +1,17 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
-from .views import ListingViewSet, BookingViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-router = DefaultRouter()
-router.register(r'listings', ListingViewSet)
-router.register(r'bookings', BookingViewSet)
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Travel App API",
+      default_version='v1',
+      description="API documentation for Listings and Bookings",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
-urlpatterns = [
-    path('', include(router.urls)),
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
