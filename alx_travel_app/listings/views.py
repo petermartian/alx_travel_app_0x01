444f0f2cdd721b372009d17ac718de5e02758d8a
Peter Martian <petmartian@gmail.com>
2025-08-01 18:06:55 +0100

    Update views.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
-from .models import Listing
-from .serializers import ListingSerializer
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class ListingViewSet(viewsets.ModelViewSet):
-    queryset = Listing.objects.all().order_by('-created_at')
+    queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
